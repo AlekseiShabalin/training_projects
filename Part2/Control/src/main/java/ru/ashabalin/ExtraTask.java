e3dacba694e170fbe702dbe54e249eb62ae7a8ad
Aleksei Shabalin <av.shabalin85@gmail.com>
2016-10-05 23:17:20 +0400

    update ExtraTask

--- Part2/Control/src/main/java/ru/ashabalin/ExtraTask.java
+++ Part2/Control/src/main/java/ru/ashabalin/ExtraTask.java
@@ -4,7 +4,6 @@ package ru.ashabalin;
  * @author Aleksei Shabalin on 03.10.2016.
  */
 public class ExtraTask {
-	private int longArray;
 	private int array[];
 
 	public int[] getArray() {
@@ -12,27 +11,37 @@ public class ExtraTask {
 	}
 
 	public void mergerArray(int array1[], int array2[]){
-		this.longArray = array1.length + array2.length;
-		this.array = new int[longArray];
+		int indexArray1 = 0;
+		int indexArray2 = 0;
+		int index = 0;
+		this.array = new int[array1.length + array2.length];
 
-		for(int i = 0; i < array1.length; i++){
-			this.array[i] = array1[i];
-		}
-
-		for(int j = 0; j < array2.length; j++){
-			this.array[array1.length + j] = array2[j];
-		}
-
-		// sort bubble
-		for(int i = array.length - 1; i > 0; i--){
-			for(int j = 0; j < i; j++){
-				if(array[j] > array[j + 1]){
-					int a = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = a;
-				}
-
+		// сравниваем элементы массивов пока есть хоть один необработанный элемент
+		while(indexArray1 < array1.length && indexArray2 < array2.length){
+			// меньший из элементов массива записываем в
+			// результирующий маассив, обновляем нужный индек массива
+			if(array1[indexArray1] < array2[indexArray2]){
+				array[index] = array1[indexArray1];
+				indexArray1++;
+			}else{
+				array[index] = array2[indexArray2];
+				indexArray2++;
 			}
+			index++;
+		}
+
+		// оставшиеся элементы одного из массивов
+		// дописываем в результирующий массив
+		while (indexArray1 < array1.length){
+			array[index] = array1[indexArray1];
+			index++;
+			indexArray1++;
+		}
+
+		while (indexArray2 < array2.length){
+			array[index] = array2[indexArray2];
+			index++;
+			indexArray2++;
 		}
 
 	}
