e8714722cad54c5fc42ddeed1c663d33fd7cf43d
Aleksei Shabalin <av.shabalin85@gmail.com>
2016-10-24 23:15:30 +0400

    add MinimumNumberTest

--- Part3/Lesson1/src/test/java/ru/ashabalin/MinimumNumberTest.java
+++ Part3/Lesson1/src/test/java/ru/ashabalin/MinimumNumberTest.java
@@ -1,8 +1,24 @@
-import static org.junit.Assert.*;
+package ru.ashabalin;
+
+import org.junit.Test;
+import java.io.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Aleksei Shabalin on 22.10.2016.
  */
-public class MinimumNumberTest {
+
+public class MinimumNumberTest{
+
+	@Test
+	public void minimumNumberTest() throws Exception {
+
+		MinimumNumber minimumNumberTest = new MinimumNumber();
+		String testString = "-10.25 15 24";
+		System.setIn(new ByteArrayInputStream(testString.getBytes()));
+		double resultTest = minimumNumberTest.minimumNumber();
+		assertEquals(-10.25d, resultTest, 0.000000000001);
+
+	}
 
 }
